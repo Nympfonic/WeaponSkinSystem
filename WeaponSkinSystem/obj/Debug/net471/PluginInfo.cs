c9ff6d5910c58dcad7f5bf0745d422177e4c1875
Nympfonic <arys.steam@gmail.com>
2024-04-19 19:53:39 +0100

    Add project files.

---
+++ WeaponSkinSystem/obj/Debug/net471/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Arys.WeaponSkinSystem
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Arys-WeaponSkinSystem";
+        public const string PLUGIN_NAME = "Arys-WeaponSkinSystem";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
